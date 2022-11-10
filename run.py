4afa3d863044ae6d4dc7d1cd584ec847e5a46c41
AurelienSomme <aure.somme@hotmail.com>
2022-11-10 14:03:35 +0100

    Update

---
+++ run.py
@@ -0,0 +1,46 @@
+import json
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+        
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/pictures')
+def pictures():
+        return {'nb_ppl' : nb_ppl, 'nb_man' : nb_man, 'nb_women' : nb_women, 'average_age_man' : average_age_man, 'average_age_women' : average_age_women, 'average_age' : average_age, 'ages_man' : ages_man, 'ages_women' : ages_women, 'ages' : ages}
+
+if __name__ == "__main__":
+    
+
+    nb_man = 0
+    nb_women = 0
+    nb_ppl = 0
+    ages_man = []
+    ages_women = []
+    #Indiquer tous les fichiers json utilisés
+    name_files = ["picture1.json", "picture2.json", "picture3.json"]
+
+    for i in range(len(name_files)):
+        file = open(name_files[i])
+        data = json.load(file)
+
+        for face in data['response']['faces']:
+            if face['gender'] == 'male':
+                nb_man += 1
+                ages_man.append(int(face['age']))
+            else:
+                nb_women += 1
+                ages_women.append(int(face['age']))
+
+        nb_ppl += data['response']['face_count']
+
+    average_age_man = sum(ages_man) / nb_man
+    average_age_women = sum(ages_women) / nb_women
+    average_age = (average_age_man + average_age_women) / 2
+    ages = ages_women + ages_man
+
+    app.run(debug=True)
